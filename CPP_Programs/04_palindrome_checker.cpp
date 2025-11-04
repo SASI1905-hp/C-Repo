b46daea83c569e924a7f88b0e93b3fdc2cefe139
CEO_OF_UNIVERSE <kondasrinu260@gmail.com>
2025-11-04 21:32:36 +0530

    Create 04_palindrome_checker.cpp

---
+++ CPP_Programs/04_palindrome_checker.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string s) {
+    int n = s.length();
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[n - 1 - i])
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+    
+    if (isPalindrome(str))
+        cout << str << " is a palindrome" << endl;
+    else
+        cout << str << " is not a palindrome" << endl;
+    
+    return 0;
+}
